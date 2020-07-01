b3ac223eb395b04d1d7aa699e815250e392745d4
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2020-07-01 01:12:59 -0400

    add image

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_202229) do
+ActiveRecord::Schema.define(version: 2020_06_30_211308) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image_url"
   end
 
   create_table "ideas", force: :cascade do |t|
--- db/seeds/ecommerce.rb
+++ db/seeds/ecommerce.rb
@@ -1,16 +1,17 @@
 
-
-cat = Category.find_or_create_by(name: "black top")
+Category.delete_all
+Product.delete_all
+cat = Category.find_or_create_by(name: "black top", image_url: "https://colorlib.com/preview/theme/fashi/img/banner-1.jpg")
 Product.find_or_create_by(name: "small black top", price: 23.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/woman-1477091_1920-255x320.jpg")
 Product.find_or_create_by(name: "little black top", price: 18.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/girl-1286993_1920-255x320.jpg")
 Product.find_or_create_by(name: "nice black top", price: 13.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/model-858751_1920-255x320.jpg")
 
-cat = Category.find_or_create_by(name: "high heel shoes")
+cat = Category.find_or_create_by(name: "high heel shoes", image_url: "https://colorlib.com/preview/theme/fashi/img/banner-2.jpg")
 Product.find_or_create_by(name: "small black top", price: 23.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/elegance-18594_1920-255x320.jpg")
 Product.find_or_create_by(name: "little black top", price: 43.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/shoe-1040802_1920-255x320.jpg")
 Product.find_or_create_by(name: "nice black top", price: 33.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/high-heels-1327020_1920-255x320.jpg")
 
-cat = Category.find_or_create_by(name: "shirts and blouse")
+cat = Category.find_or_create_by(name: "shirts and blouse", image_url: "https://colorlib.com/preview/theme/fashi/img/banner-3.jpg")
 Product.find_or_create_by(name: "mango shirt", price: 23.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/cute-955782_1920-255x320.jpg", featured: true)
 Product.find_or_create_by(name: "blue sweater", price: 43.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/fashion-1283863_1920-255x320.jpg", featured: true)
 Product.find_or_create_by(name: "asabi-jeans", price: 33.40, category_id: cat.id, image_url: "https://colorlib.com/tyche/wp-content/uploads/sites/17/2017/06/person-1148941_1920-255x320.jpg", featured: true)
